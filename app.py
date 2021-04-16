e306b9e7e35ab3db2d5d4daa2745985a1c09b821
Sam199971 <s88070171@gmail.com>
2021-04-16 15:50:37 +0800

    2021/04/16

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pyodbc
-
-
-
+from jinja2 import Template
 
 app = Flask(__name__)
 
+# 連接mssql
 
-
-#連接mssql
-
-server = 'LAPTOP-5GRQ8RBR\SQLEXPRESS'
+server = 'LAPTOP-US1I0DLQ\SQLEXPRESS'
 database = 'SQL_Class'
 username = 'sa'
-password = 'micky5630'
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+password = 'f129960349'
+
+sqlConn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+
+cursor = sqlConn.cursor()
 
 
-cursor = cnxn.cursor()
-cursor.execute("select * from Product")
-rows = cursor.fetchall()
-for row in rows:
-    print (row.pName)
+# cursor.execute("select * from Product")
+# rows = cursor.fetchall()
+#
+# for row in rows:
+#     print(row.pName)
 
 
-
-
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def hello_world():
-    return render_template("home.html")
+
+    cursor.execute("Select sType From [Space] GROUP BY sType")
+    types = cursor.fetchall()
+
+    if request.method == 'POST':
+        date = request.form['bday']
+        sType = request.form['sel']
+        print(date)
+        print(sType)
+
+        cursor.execute("""Select *
+		                        From SpaceTime as ST inner join [Space] as S on ST.sId = S.[sid]
+		                        Where Date = ?  and S.sType = ?""",  date , sType)
+        DateRecord = cursor.fetchall()
+        print(DateRecord)
+
+        return render_template('Search.html', types=types, date = date, sel = sType)
+    else:
+        return render_template('Search.html', types=types)
+
 
 
 if __name__ == '__main__':
